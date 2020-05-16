c150c440ac8bbe86a9f70d38740038fc816b4621
Theone Eclarin <Theone_Eclarin@outlook.com>
2020-05-15 23:54:31 -0700

    Added Sample4

---
+++ DWNavigationPane/Samples/Sample4/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using DWNavigationPane;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sample4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string previousSenderText = "Explorer";
+
+        private void PaneButton_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement element = sender as FrameworkElement;
+            if (!MainPanel.IsOpen)
+            {
+                MainPanel.IsOpen = true;
+            }
+            else if (previousSenderText == (string)element.ToolTip)
+            {
+                MainPanel.IsOpen = false;
+                return;
+            }
+            MainPanelTitle.Text = ((string)element.ToolTip).ToUpper();
+            Item.SetIcon(MainPanelIconHolder, Item.GetIcon(element));
+            previousSenderText = (string)element.ToolTip;
+        }
+    }
+}
