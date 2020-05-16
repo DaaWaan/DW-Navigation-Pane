11efddcdecee7cffe38e237a3d02ddfb5b3793ed
Theone Eclarin <Theone_Eclarin@outlook.com>
2020-05-16 05:10:13 -0700

    Edit grammar in assembly information

--- DWNavigationPane/DWNavigationPane/Properties/AssemblyInfo.cs
+++ DWNavigationPane/DWNavigationPane/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DWNavigationPane")]
-[assembly: AssemblyDescription("A framework for modern navigation panes and its items for making a better UI for their own WPF applications.")]
+[assembly: AssemblyDescription("A framework for modern navigation panes and its items for making a better UI for WPF applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Theone Genesis Eclarin")]
 [assembly: AssemblyProduct("DWNavigationPane")]
@@ -48,4 +48,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
